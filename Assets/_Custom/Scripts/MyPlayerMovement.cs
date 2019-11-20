b349e75a5f0a1e5748fd1ebced2302b14d9cc23f
vquanghuy <vquanghuy@gmail.com>
2019-11-20 11:33:24 +0700

    Player movement

---
+++ Assets/_Custom/Scripts/MyPlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;   
+
+    Vector3 movement;          
+    Animator anim;             
+    Rigidbody playerRigidbody; 
+    int floorMask;             
+    float camRayLength = 100f; 
+    void Awake() {
+        // Create a layer mask for the floor layer.
+        floorMask = LayerMask.GetMask ("Floor");
+
+        // Set up references.
+        anim = GetComponent <Animator> ();
+        playerRigidbody = GetComponent <Rigidbody> ();
+    }
+
+    void FixedUpdate() {
+        float h = Input.GetAxisRaw ("Horizontal");
+        float v = Input.GetAxisRaw ("Vertical");
+
+        Move (h, v);
+        Animating(h, v);
+    }
+
+    void Move (float h, float v)
+    {
+        // Set the movement vector based on the axis input.
+        movement.Set (h, 0f, v);
+
+        // Normalise the movement vector and make it proportional to the speed per second.
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        // Move the player to it's current position plus the movement.
+        playerRigidbody.MovePosition (transform.position + movement);
+    }
+
+    void Animating (float h, float v)
+    {
+        // Create a boolean that is true if either of the input axes is non-zero.
+        bool walking = h != 0f || v != 0f;
+
+        // Tell the animator whether or not the player is walking.
+        anim.SetBool ("IsWalking", walking);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
